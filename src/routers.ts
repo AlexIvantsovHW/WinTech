2523fff78c54fe19dc2d3640ccdadf2f7e22c8f6
AlexIvantsovHW <alexander.ivantsov.hw@gmail.com>
2024-08-30 19:56:24 +0300

    feat: add enum with routes

---
+++ src/routers.ts
@@ -0,0 +1,9 @@
+export enum ROUTER {
+  HOME = "/",
+  ABOUT = "/about",
+  CONTACT = "/contact",
+  NEWEST_GAMES = "/newest-games",
+  ALL_GAMES = "/all-games",
+  ABOUT_US = "/about-us",
+  CONTACT_US = "/contact-us",
+}
